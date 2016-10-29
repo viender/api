da9b590fb772a9eb30be5e615c72e3fe616e4a80
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-10-30 01:45:25 +0700

    install passport

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -6,7 +6,7 @@
                     <div class="panel-heading">Example Component</div>
 
                     <div class="panel-body">
-                        Im a Test
+                        I know Test
                     </div>
                 </div>
             </div>
