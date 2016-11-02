87fe13a95adf4734a135e8ec01e63178807dae22
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-02 17:22:19 +0700

    change app name to viender

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,15 +81,15 @@
 
             <div class="content" id="app">
                 <div class="title m-b-md">
-                    Laravel
+                    Viender
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
+{{--                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> --}}
                 </div>
             </div>
         </div>
