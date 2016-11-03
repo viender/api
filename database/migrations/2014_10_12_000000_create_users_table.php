87d2f18f53ccae4471c4c05c6cbe064d94c1747a
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-03 14:59:41 +0700

    make gender nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('gender');
+            $table->string('gender')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
