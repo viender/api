a8c792c98dea6e9c3e7bd79002ee4d6213192c1b
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-04 15:42:39 +0700

    add upvote factory

---
+++ database/factories/UpvoteFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Upvote::class, function (Faker\Generator $faker) {
+
+    $users = App\User::all()->pluck('id')->toArray();
+
+    $upvotables = [
+        'App\Question',
+        'App\Answer',
+        'App\Comment',
+    ];
+
+    return [
+        'user_id' => $faker->randomElement($users),
+        'upvotable_type' => $faker->randomElement($upvotables),
+        'upvotable_id' => function(array $me) {
+            return $faker->randomElement($me['upvotable_type']::all()->pluck('id')->toArray());
+        },
+    ];
+});
