38e7a074b13404cc6b6d1a67f1dacd82eb0ce1cd
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-04 17:00:47 +0700

    change body type to text

--- database/migrations/2016_11_02_102402_create_answers_table.php
+++ database/migrations/2016_11_02_102402_create_answers_table.php
@@ -20,7 +20,7 @@ class CreateAnswersTable extends Migration
             $table->integer('question_id')->unsigned();
             $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->string('title');
-            $table->string('body');
+            $table->text('body');
             $table->timestamps();
         });
     }
