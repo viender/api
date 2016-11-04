393f8ca4c1f2115ab0f3aeb4f5202c1be17a5ad4
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-04 17:00:24 +0700

    rename to tag_user

--- database/migrations/2016_11_03_063615_create_tag_user_table.php
+++ database/migrations/2016_11_03_063615_create_tag_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersFollowsTagsTable extends Migration
+class CreateTagUserTable extends Migration
 {
     /**
      * Run the migrations.
