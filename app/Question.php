a321982dd531713844b3f73fd7ca68ebf323d300
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-05 13:05:02 +0700

    change route key name to slug

--- app/Question.php
+++ app/Question.php
@@ -14,6 +14,16 @@ class Question extends Model
     protected $fillable = [
         'title', 'body',
     ];
+    
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     public function user() 
     {
