81764f75c635de6a826db317ab7496ec51bbf8c9
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-05 10:28:25 +0700

    assign tag when seeded

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -11,6 +11,23 @@ class QuestionsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Question::class, 50)->create();
+        factory(App\Question::class, 50)->create()->each(function($u) {
+            
+            $faker = Faker\Factory::create();
+            
+            $tags = App\Tag::all()->pluck('id')->toArray();
+
+            $tag = App\Tag::find($faker->randomElement($tags));
+
+            $u->tags()->save($tag);
+
+            foreach (range(1, 2) as $i) {
+                if(rand(0, 1)) {
+                    $tag = App\Tag::find($faker->randomElement($tags));
+
+                    $u->tags()->save($tag);
+                }
+            }
+        });
     }
 }
