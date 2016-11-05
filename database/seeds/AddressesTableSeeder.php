862000e774431a1114e1f0d5be2e13c4fa3840f6
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-05 10:26:37 +0700

    fix commentable always null when called from seeder

--- database/seeds/AddressesTableSeeder.php
+++ database/seeds/AddressesTableSeeder.php
@@ -11,6 +11,24 @@ class AddressesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Address::class, 1000)->create();
+        $faker = Faker\Factory::create();
+        $cities = App\City::all()->pluck('id')->toArray();
+
+        foreach (App\User::all() as $user) {
+            $street = App\Street::create([
+                'city_id' => $faker->randomElement($cities),
+                'name' => $faker->streetAddress,    
+            ]);
+
+            App\Address::create([
+                'user_id' => $user->id,
+                'street_id' => $street->id,
+                'city_id' => $street->city_id,
+                'state_id' => $street->state()->first()->id,
+                'country_id' => $street->country()->first()->id,
+                'active' => $faker->randomElement([true, false]),
+            ]);
+        }
+        // factory(App\Address::class, 1000)->create();
     }
 }
