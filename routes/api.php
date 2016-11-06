b32e5f434944ea4736dd511204e56861a58b5a88
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-06 23:57:49 +0700

    exclude create and edit method

--- routes/api.php
+++ routes/api.php
@@ -5,76 +5,76 @@ use Illuminate\Http\Request;
 Route::group(['namespace' => 'Api\Version1', 'middleware' => 'auth:api'], function() {
 
     // Models
-    Route::resource('users', 'UsersController');
-    Route::resource('stars', 'StarsController');
-    Route::resource('addresses', 'AddressesController');
-    Route::resource('countries', 'CountriesController');
-    Route::resource('states', 'StatesController');
-    Route::resource('cities', 'CitiesController');
-    Route::resource('streets', 'StreetsController');
-    Route::resource('answers', 'AnswersController');
-    Route::resource('questions', 'QuestionsController');
-    Route::resource('auctions', 'AuctionsController');
-    Route::resource('bids', 'BidsController');
-    Route::resource('comments', 'CommentsController');
-    Route::resource('tags', 'TagsController');
-    Route::resource('upvotes', 'UpvotesController');
-    Route::resource('downvotes', 'DownvotesController');
+    Route::resource('users', 'UsersController', ['except' => ['create', 'edit']]);
+    Route::resource('stars', 'StarsController', ['except' => ['create', 'edit']]);
+    Route::resource('addresses', 'AddressesController', ['except' => ['create', 'edit']]);
+    Route::resource('countries', 'CountriesController', ['except' => ['create', 'edit']]);
+    Route::resource('states', 'StatesController', ['except' => ['create', 'edit']]);
+    Route::resource('cities', 'CitiesController', ['except' => ['create', 'edit']]);
+    Route::resource('streets', 'StreetsController', ['except' => ['create', 'edit']]);
+    Route::resource('answers', 'AnswersController', ['except' => ['create', 'edit']]);
+    Route::resource('questions', 'QuestionsController', ['except' => ['create', 'edit']]);
+    Route::resource('auctions', 'AuctionsController', ['except' => ['create', 'edit']]);
+    Route::resource('bids', 'BidsController', ['except' => ['create', 'edit']]);
+    Route::resource('comments', 'CommentsController', ['except' => ['create', 'edit']]);
+    Route::resource('tags', 'TagsController', ['except' => ['create', 'edit']]);
+    Route::resource('upvotes', 'UpvotesController', ['except' => ['create', 'edit']]);
+    Route::resource('downvotes', 'DownvotesController', ['except' => ['create', 'edit']]);
 
 
     // Relationships
     // Relationships - Ask
-    Route::resource('users.questions', 'UserQuestionsController');
-    Route::resource('users.answers', 'UserAnswersController');
-    Route::resource('questions.answers', 'QuestionAnswersController');
+    Route::resource('users.questions', 'UserQuestionsController', ['except' => ['create', 'edit']]);
+    Route::resource('users.answers', 'UserAnswersController', ['except' => ['create', 'edit']]);
+    Route::resource('questions.answers', 'QuestionAnswersController', ['except' => ['create', 'edit']]);
 
 
     // Relationships - Tag
-    Route::resource('users.tags', 'UserTagsController');
-    Route::resource('questions.tags', 'QuestionTagsController');
-    Route::resource('auctions.tags', 'AuctionTagsController');
+    Route::resource('users.tags', 'UserTagsController', ['except' => ['create', 'edit']]);
+    Route::resource('questions.tags', 'QuestionTagsController', ['except' => ['create', 'edit']]);
+    Route::resource('auctions.tags', 'AuctionTagsController', ['except' => ['create', 'edit']]);
     
-    Route::resource('tags.users', 'TagUsersController');
-    Route::resource('tags.questions', 'TagQuestionsController');
-    Route::resource('tags.auctions', 'TagAuctionsController');
+    Route::resource('tags.users', 'TagUsersController', ['except' => ['create', 'edit']]);
+    Route::resource('tags.questions', 'TagQuestionsController', ['except' => ['create', 'edit']]);
+    Route::resource('tags.auctions', 'TagAuctionsController', ['except' => ['create', 'edit']]);
 
 
     // Relationships - Comment
-    Route::resource('users.comments', 'UserCommentsController');
-    Route::resource('questions.comments', 'QuestionCommentsController');
-    Route::resource('answers.comments', 'AnswerCommentsController');
-    Route::resource('comments/{id}/comments', 'CommentCommentsController');
+    Route::resource('users.comments', 'UserCommentsController', ['except' => ['create', 'edit']]);
+    Route::resource('questions.comments', 'QuestionCommentsController', ['except' => ['create', 'edit']]);
+    Route::resource('answers.comments', 'AnswerCommentsController', ['except' => ['create', 'edit']]);
+    Route::resource('comments/{id}/comments', 'CommentCommentsController', ['except' => ['create', 'edit']]);
 
 
     // Relationships - Vote
-    Route::resource('users.upvotes', 'UserUpvotesController');
-    Route::resource('questions.upvotes', 'QuestionUpvotesController');
-    Route::resource('answers.upvotes', 'AnswerUpvotesController');
-    Route::resource('comments.upvotes', 'CommentUpvotesController');
+    Route::resource('users.upvotes', 'UserUpvotesController', ['except' => ['create', 'edit']]);
+    Route::resource('questions.upvotes', 'QuestionUpvotesController', ['except' => ['create', 'edit']]);
+    Route::resource('answers.upvotes', 'AnswerUpvotesController', ['except' => ['create', 'edit']]);
+    Route::resource('comments.upvotes', 'CommentUpvotesController', ['except' => ['create', 'edit']]);
 
-    Route::resource('users.downvotes', 'UserDownvotesController');
-    Route::resource('questions.downvotes', 'QuestionDownvotesController');
-    Route::resource('answers.downvotes', 'AnswerDownvotesController');
-    Route::resource('comments.downvotes', 'CommentDownvotesController');
+    Route::resource('users.downvotes', 'UserDownvotesController', ['except' => ['create', 'edit']]);
+    Route::resource('questions.downvotes', 'QuestionDownvotesController', ['except' => ['create', 'edit']]);
+    Route::resource('answers.downvotes', 'AnswerDownvotesController', ['except' => ['create', 'edit']]);
+    Route::resource('comments.downvotes', 'CommentDownvotesController', ['except' => ['create', 'edit']]);
 
 
     // Relationships - Tutor seeker
-    Route::resource('users.auctions', 'UserAuctionsController');
-    Route::resource('users.bids', 'UserBidsController');
-    Route::resource('auctions.bids', 'AuctionBidsController');
+    Route::resource('users.auctions', 'UserAuctionsController', ['except' => ['create', 'edit']]);
+    Route::resource('users.bids', 'UserBidsController', ['except' => ['create', 'edit']]);
+    Route::resource('auctions.bids', 'AuctionBidsController', ['except' => ['create', 'edit']]);
 
 
     // Relationships - Tutor rating
-    Route::resource('users.stars', 'UserStarsController');
+    Route::resource('users.stars', 'UserStarsController', ['except' => ['create', 'edit']]);
 
 
     // Relationships - User address
-    Route::resource('users.addresses', 'UserAddressesController');
-    Route::resource('countries.addresses', 'CountryAddressesController');
-    Route::resource('countries.states', 'CountryStatesController');
-    Route::resource('states.addresses', 'StateAddressesController');
-    Route::resource('states.cities', 'StateCitiesController');
-    Route::resource('cities.addresses', 'CityAddressesController');
-    Route::resource('cities.streets', 'CityStreetsController');
-    Route::resource('streets.addresses', 'StreetAddressesController');
+    Route::resource('users.addresses', 'UserAddressesController', ['except' => ['create', 'edit']]);
+    Route::resource('countries.addresses', 'CountryAddressesController', ['except' => ['create', 'edit']]);
+    Route::resource('countries.states', 'CountryStatesController', ['except' => ['create', 'edit']]);
+    Route::resource('states.addresses', 'StateAddressesController', ['except' => ['create', 'edit']]);
+    Route::resource('states.cities', 'StateCitiesController', ['except' => ['create', 'edit']]);
+    Route::resource('cities.addresses', 'CityAddressesController', ['except' => ['create', 'edit']]);
+    Route::resource('cities.streets', 'CityStreetsController', ['except' => ['create', 'edit']]);
+    Route::resource('streets.addresses', 'StreetAddressesController', ['except' => ['create', 'edit']]);
 });
