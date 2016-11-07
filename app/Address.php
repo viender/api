92f392e535e732ce50dce269f422e5ba569d483f
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-07 15:10:50 +0700

    remove country_id state_id and city_id

--- app/Address.php
+++ app/Address.php
@@ -12,7 +12,7 @@ class Address extends Model
      * @var array
      */
     protected $fillable = [
-        'user_id', 'street_id', 'city_id', 'state_id', 'country_id',
+        'user_id', 'street_id',
     ];
 
     public function user() 
