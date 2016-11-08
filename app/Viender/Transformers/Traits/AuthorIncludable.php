a47ccfc85a1dfe7a6825527d87d2851f54b0627b
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-08 16:52:22 +0700

    add UserIncludable trait

---
+++ app/Viender/Transformers/Traits/AuthorIncludable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Viender\Transformers\Traits;
+
+use App\Viender\Transformers\UserTransformer;
+
+trait AuthorIncludable
+{
+    /**
+     * Include Author
+     *
+     * @return League\Fractal\ItemResource
+     */
+    public function includeAuthor($item)
+    {
+        $author = $item->user;
+
+        return $this->item($author, new UserTransformer);
+    }
+}
