c026ef1d1c2d42389d6a128264e7517b4ecc2630
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-08 15:29:26 +0700

    implement index show create update and delete methods

--- app/Http/Controllers/Api/Version1/AddressesController.php
+++ app/Http/Controllers/Api/Version1/AddressesController.php
@@ -4,73 +4,141 @@ namespace App\Http\Controllers\Api\Version1;
 
 use App\Address;
 use Illuminate\Http\Request;
+use League\Fractal\Resource\Item;
+use App\Viender\Transformers\AddressTransformer;
 
 class AddressesController extends ApiController
 {
-    /**
-     * Display a listing of the resource.
+    /** 
+     * @api {get} /addresses Get Addresses
+     * @apiName AddressIndex
+     * @apiGroup Address
+     * @apiVersion 1.0.0
+     * @apiDescription Get a page of Addresses
      *
+     * @apiHeader {String} Content-Type Content-Type
+     * 
+     * @apiSuccess {Object[]} data Array of addresses
+     * @apiSuccess {Number} data.id Adresses unique ID
+     * @apiSuccess {String} data.street Street name
+     * @apiSuccess {String} data.city City name
+     * @apiSuccess {String} data.state State name
+     * @apiSuccess {String} data.country Country name
+     * @apiSuccess {Object[]} data.links Addresses links
+     * @apiSuccess {Url} data.links.rel Addresses links rel
+     * @apiSuccess {Url} data.links.uri Addresses links uri
+     * 
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return Address::all();
+        $paginator = Address::with('street.city.state.country')->paginate();
+        return $this->respondWithPagination($paginator, new AddressTransformer);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @api {post} /addresses Create Address
+     * @apiName AddressStore
+     * @apiGroup Address
+     * @apiVersion 1.0.0
+     * @apiDescription Create a new Addresses
      *
+     * @apiHeader {String} Authorization Personal Access Token
+     * @apiHeader {String} Content-Type Content-Type
+     * 
+     * @apiParam (Request Body Fields) {Number} user_id Users unique ID
+     * @apiParam (Request Body Fields) {Number} city_id Cities unique ID
+     * @apiParam (Request Body Fields) {String} name Street name
+     *
+     * @apiSuccess {String} message Response message
+     * @apiSuccess {Number} status_code Response status code
+     * 
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $address = Address::create($request->all());
-
-        return 'success';
+        Address::createWithStreet($request->all());
+        return $this->respondCreated();
     }
 
     /**
-     * Display the specified resource.
+     * @api {get} /addresses/:id Get Address by ID
+     * @apiName AddressShow
+     * @apiGroup Address
+     * @apiVersion 1.0.0
+     * @apiDescription Get an Addresses object
      *
+     * @apiHeader {String} Content-Type Content-Type
+     *
+     * @apiParam (Path Parameters) {Number} id Addresses unique ID
+     *
+     * @apiSuccess {Object} data Array of addresses
+     * @apiSuccess {Number} data.id Adresses unique ID
+     * @apiSuccess {String} data.street Street name
+     * @apiSuccess {String} data.city City name
+     * @apiSuccess {String} data.state State name
+     * @apiSuccess {String} data.country Country name
+     * @apiSuccess {Object[]} data.links Addresses links
+     * @apiSuccess {Url} data.links.rel Addresses links rel
+     * @apiSuccess {Url} data.links.uri Addresses links uri
+     * 
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show(Address $address)
     {
-        return [
-            'data' => $address,
-        ];
+        return $this->respond(new Item($address, new AddressTransformer));
     }
 
     /**
-     * Update the specified resource in storage.
+     * @api {put} /addresses/:id Update Address
+     * @apiName AddressUpdate
+     * @apiGroup Address
+     * @apiVersion 1.0.0
+     * @apiDescription Update an Addresses
      *
+     * @apiHeader {String} Content-Type Content-Type
+     *
+     * @apiParam (Path Parameters) {Number} id Addresses unique ID
+     *
+     * @apiParam (Request Body Fields) {Number} user_id Users unique ID
+     * @apiParam (Request Body Fields) {Number} city_id Cities unique ID
+     * @apiParam (Request Body Fields) {String} name Street name
+     * 
+     * @apiSuccess {String} message Response message
+     * @apiSuccess {Number} status_code Response status code
+     * 
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Address $address)
     {
-        return [
-            'message' => 'this is update',
-            'data' => $address,
-            'request' => $request->all(),
-        ];
+        $address->update($request->all());
+        return $this->respondUpdated();
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @api {delete} /addresses/:id Delete Address
+     * @apiName AddressDelete
+     * @apiGroup Address
+     * @apiVersion 1.0.0
+     * @apiDescription Delete an Addresses
      *
+     * @apiHeader {String} Content-Type Content-Type
+     *
+     * @apiParam (Path Parameters) {Number} id Addresses unique ID
+     *
+     * @apiSuccess {String} message Response message
+     * @apiSuccess {Number} status_code Response status code
+     * 
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy(Address $address)
     {
-        return [
-            'message' => 'this is destroy',
-            'data' => $address,
-            'request' => $request->all(),
-        ];
+        $address->delete();
+        return $this->respondDeleted();
     }
 }
