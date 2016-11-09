92431b0a274b70760cfd6459129b8858bb2de68c
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-09 11:52:22 +0700

    add handlers

---
+++ app/Http/Controllers/Api/Version1/Handlers/Handler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Version1\Handlers;
+
+use Illuminate\Http\Request;
+use League\Fractal\Resource\Item;
+
+class Handler
+{
+    protected $controller;
+    protected $model;
+    protected $transformer;
+
+    public function __construct($controller, $model, $transformer)
+    {
+        $this->controller = $controller;
+        $this->model = $model;
+        $this->transformer = $transformer;
+    }
+}
