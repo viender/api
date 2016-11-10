d5fef766966a081ef1a8425d651eb51041a263bf
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-10 23:46:31 +0700

    implement index, store, show, update and delete method

--- app/Http/Controllers/Api/Version1/CommentDownvotesController.php
+++ app/Http/Controllers/Api/Version1/CommentDownvotesController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Api\Version1;
 
+use App\Comment;
+use App\Downvote;
 use Illuminate\Http\Request;
+use League\Fractal\Resource\Item;
+use App\Viender\Transformers\Version1\DownvoteTransformer;
 
 class CommentDownvotesController extends ApiController
 {
@@ -11,9 +15,11 @@ class CommentDownvotesController extends ApiController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Comment $comment)
     {
-        //
+        $paginator = $comment->downvotes()->paginate();
+
+        return $this->respondWithPagination($paginator, new DownvoteTransformer);
     }
 
     /**
@@ -22,9 +28,11 @@ class CommentDownvotesController extends ApiController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Comment $comment)
     {
-        //
+        $comment->downvotes()->save(new Downvote($request->all()));
+
+        return $this->respondCreated();
     }
 
     /**
@@ -33,9 +41,11 @@ class CommentDownvotesController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comment $comment, $downvote)
     {
-        //
+        $downvote = $comment->downvotes()->findOrFail($downvote);
+
+        return $this->respond(new Item($downvote, new DownvoteTransformer));
     }
 
     /**
@@ -45,9 +55,13 @@ class CommentDownvotesController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comment $comment, $downvote)
     {
-        //
+        $downvote = $comment->downvotes()->findOrFail($downvote);
+
+        $downvote->update($request->all());
+
+        return $this->respondUpdated();
     }
 
     /**
@@ -56,8 +70,12 @@ class CommentDownvotesController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Comment $comment, $downvote)
     {
-        //
+        $downvote = $comment->downvotes()->findOrFail($downvote);
+        
+        $downvote->delete();
+
+        return $this->respondDeleted();
     }
 }
