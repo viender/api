db2d4e303cd93d549fffb5f92594439fc040c5c0
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-11 00:26:53 +0700

    implement index, store, show, update and destroy methods

--- app/Http/Controllers/Api/Version1/StateCitiesController.php
+++ app/Http/Controllers/Api/Version1/StateCitiesController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Api\Version1;
 
+use App\State;
+use App\City;
 use Illuminate\Http\Request;
+use League\Fractal\Resource\Item;
+use App\Viender\Transformers\Version1\CityTransformer;
 
 class StateCitiesController extends ApiController
 {
@@ -11,9 +15,11 @@ class StateCitiesController extends ApiController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(State $state)
     {
-        //
+        $paginator = $state->cities()->paginate();
+
+        return $this->respondWithPagination($paginator, new CityTransformer);
     }
 
     /**
@@ -22,9 +28,11 @@ class StateCitiesController extends ApiController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, State $state)
     {
-        //
+        $state->cities()->save(new City($request->all()));
+
+        return $this->respondCreated();
     }
 
     /**
@@ -33,9 +41,11 @@ class StateCitiesController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(State $state, $city)
     {
-        //
+        $city = $state->cities()->findOrFail($city);
+
+        return $this->respond(new Item($city, new CityTransformer));
     }
 
     /**
@@ -45,9 +55,13 @@ class StateCitiesController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, State $state, $city)
     {
-        //
+        $city = $state->cities()->findOrFail($city);
+
+        $city->update($request->all());
+
+        return $this->respondUpdated();
     }
 
     /**
@@ -56,8 +70,12 @@ class StateCitiesController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(State $state, $city)
     {
-        //
+        $city = $state->cities()->findOrFail($city);
+        
+        $city->delete();
+
+        return $this->respondDeleted();
     }
 }
