dd454c2b22183bda10f02c2ec1b307a5347e2f6f
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-14 10:42:38 +0700

    test

--- app/Viender/Utilities/Text.php
+++ app/Viender/Utilities/Text.php
@@ -5,6 +5,7 @@ namespace App\Viender\Utilities;
 class Text {
     public static function clean($string) 
     {
+        
         $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
 
         return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
