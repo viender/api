12aca869b0effca4ff8aa5e58ebacbe0e883936f
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-18 10:19:39 +0700

    make user_id fillable

--- app/Downvote.php
+++ app/Downvote.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Downvote extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+    ];
+
     public function user() 
     {
         return $this->belongsTo('App\User');
--- app/Upvote.php
+++ app/Upvote.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Upvote extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+    ];
+
     public function user() 
     {
         return $this->belongsTo('App\User');
