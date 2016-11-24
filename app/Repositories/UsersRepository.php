e66ad8c6dfb14fe7a712cfa6c82ceecfd1213d52
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-11-24 17:30:42 +0700

    Move to appropriate repository

--- app/Repositories/UsersRepository.php
+++ app/Repositories/UsersRepository.php
@@ -3,41 +3,12 @@
 namespace App\Repositories;
 
 use App\Upvote;
-use App\Comment;
 use App\Contracts\Post\Upvotable;
-use App\Contracts\Post\Commentable;
-use Illuminate\Database\QueryException;
 
 class UsersRepository extends Repository
 {
     public function model()
     {
         return 'App\User';
-    }
-
-    public function toggleUpvote(Upvotable $upvotable) 
-    {
-        if($upvotable->upvotes()->where('user_id', \Auth::user()->id)->exists()) {
-            $upvote = $upvotable->upvotes()->where('user_id', \Auth::user()->id)->first();
-            $upvote->delete();
-            return false;
-        }
-
-        $upvotable->upvotes()->save(new Upvote(['user_id' => \Auth::user()->id]));
-
-        return true;
-    }
-
-    public function addComment(Commentable $commentable, array $requestArray) 
-    {
-        $requestArray['user_id'] = \Auth::user()->id;
-        
-        try {
-            $commentable->comments()->save(new Comment($requestArray));
-        } catch (QueryException $e) {
-            return false;
-        }
-
-        return true;
     }
 }
