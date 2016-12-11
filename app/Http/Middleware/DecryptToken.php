66eaa1744fe9e879ff1de6e6eb13f9a22f81cd0f
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-12-12 04:10:53 +0700

    Do not decrypt tpken cookie

--- app/Http/Middleware/DecryptToken.php
+++ app/Http/Middleware/DecryptToken.php
@@ -16,19 +16,19 @@ class DecryptToken
      */
     public function handle($request, Closure $next)
     {
-        if($request->header('Authorization') !== null) {
-            $auth = explode(' ', $request->header('Authorization'));
-            $authorization = $auth[0] . ' ' . decrypt($auth[1]);
-            $request->headers->set('Authorization', $authorization);
-        }
+        // if($request->header('Authorization') !== null) {
+        //     $auth = explode(' ', $request->header('Authorization'));
+        //     $authorization = $auth[0] . ' ' . decrypt($auth[1]);
+        //     $request->headers->set('Authorization', $authorization);
+        // }
 
-        if(isset($request->refresh_token)) {
-            try{
-                $request->refresh_token = decrypt($request->refresh_token);
-            } catch(\Illuminate\Contracts\Encryption\DecryptException $e){ 
-                throw new AccessDeniedHttpException;
-            }
-        }
+        // if(isset($request->refresh_token)) {
+        //     try{
+        //         $request->refresh_token = decrypt($request->refresh_token);
+        //     } catch(\Illuminate\Contracts\Encryption\DecryptException $e){ 
+        //         throw new AccessDeniedHttpException;
+        //     }
+        // }
 
         return $next($request);
     }
