dc18f21ee0463714239f520336ca4f7efaaba968
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-12-12 14:36:37 +0700

    new header

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
      */
     'supportsCredentials' => true,
     'allowedOrigins' => ['http://web.viender.dev', 'https://web.viender.dev', 'https://viender.com', 'https://www.viender.com', 'https://dev.viender.com'],
-    'allowedHeaders' => ['content-type', 'Authorization', 'X-XSRF-TOKEN', 'X-CSRF-TOKEN'],
+    'allowedHeaders' => ['content-type', 'Authorization', 'X-XSRF-TOKEN', 'X-CSRF-TOKEN', 'Access-Control-Allow-Headers'],
     'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE'],
     'exposedHeaders' => [],
     'maxAge' => 0,
