28d70d94266c2a15839632a54da637298b3f2e8e
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2016-12-12 14:52:21 +0700

    remove header

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
      */
     'supportsCredentials' => true,
     'allowedOrigins' => ['http://web.viender.dev', 'https://web.viender.dev', 'https://viender.com', 'https://www.viender.com', 'https://dev.viender.com'],
-    'allowedHeaders' => ['Authorization', 'X-XSRF-TOKEN', 'X-CSRF-TOKEN', 'Access-Control-Allow-Headers'],
+    'allowedHeaders' => ['Authorization', 'X-XSRF-TOKEN', 'X-CSRF-TOKEN'],
     'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE'],
     'exposedHeaders' => [],
     'maxAge' => 0,
