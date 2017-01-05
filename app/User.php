cddd83b4b9bb92647d1f7a978750e9ac132bbec3
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-01-05 11:23:15 +0700

    tidy up

--- app/User.php
+++ app/User.php
@@ -2,13 +2,14 @@
 
 namespace App;
 
+use App\Ideapool\Ideapoolable;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Ideapoolable, HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -42,61 +43,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function questions()
-    {
-        return $this->hasMany('App\Ideapool\Question');
-    }
-
-    public function answers()
-    {
-        return $this->hasMany('App\Ideapool\Answer');
-    }
-
-    public function tags()
-    {
-        return $this->belongsToMany('App\Ideapool\Tag');
-    }
-
-    public function auctions() 
-    {
-        return $this->hasMany('App\Ideapool\Auction');
-    }
-
-    public function bids() 
-    {
-        return $this->hasMany('App\Ideapool\Bid');
-    }
-
-    public function comments() 
-    {
-        return $this->hasMany('App\Ideapool\Comment');
-    }
-
-    public function upvotes() 
-    {
-        return $this->hasMany('App\Ideapool\Upvote');
-    }
-
-    public function downvotes() 
-    {
-        return $this->hasMany('App\Ideapool\Downvote');
-    }
-
-    public function stars() 
-    {
-        return $this->morphMany('App\Ideapool\Star', 'starable');
-    }
-
-    public function stared() 
-    {
-        return $this->hasMany('App\Ideapool\Star');
-    }
-
-    public function addresses() 
-    {
-        return $this->hasMany('App\Ideapool\Address');
-    }
 
     public function socialAccounts() 
     {
