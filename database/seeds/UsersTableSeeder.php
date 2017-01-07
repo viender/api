12195f0792a7651828e3d153dafec660f312a61e
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-01-07 12:41:06 +0700

    fix namespace

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
 
         factory(App\User::class, 50)->create()->each(function($user) use(&$tags) {
 
-            $user->addresses()->save(factory(Viender\Ideapool\Address::class)->make());
+            $user->addresses()->save(factory(Viender\Userdata\Address::class)->make());
             
             foreach (range(1, rand(2, 5)) as $index) {
                 $user->tags()->toggle(Viender\Ideapool\Tag::find(array_rand($tags)));
