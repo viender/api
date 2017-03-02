eb86a60f356cc9a2ae9f5f6780d2a7583943dbea
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-03-02 11:23:02 +0700

    refactor structure

--- socialite/src/Contracts/Post/Commentable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Viender\Socialite\Contracts\Post;
-
-interface Commentable
-{
-    public function comments();
-}
--- socialite/src/Contracts/Post/Taggable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Viender\Socialite\Contracts\Post;
-
-Interface Taggable
-{
-    public function tags();
-}
