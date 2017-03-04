7d15cd9b89f15eec5493e9bc1c3bc4a14e906a3c
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-03-04 23:19:48 +0700

    remove mobile's menus

--- src/viender/resources/views/layouts/nav.blade.php
+++ src/viender/resources/views/layouts/nav.blade.php
@@ -1,12 +1,11 @@
 <div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-			<a class="brand-logo hide-on-med-and-down" href="{{ url('/') }}">
+			<a class="brand-logo" href="{{ url('/') }}">
 			{{ config('app.name', 'Laravel') }}
 			</a>
 		
-			<ul class="right hide-on-med-and-down">
+			<ul class="right">
 				@if (Auth::guest())
 					<li><a href="{{ url('/login') }}">Login</a></li>
 					<li><a href="{{ url('/register') }}">Register</a></li>
@@ -14,22 +13,8 @@
 					<li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
 				@endif
 			</ul>
-
-			<ul class="side-nav" id="mobile-demo">
-				<li><a href="sass.html">Sass</a></li>
-				<li><a href="badges.html">Components</a></li>
-				<li><a href="collapsible.html">Javascript</a></li>
-				<li><a href="mobile.html">Mobile</a></li>
-			</ul>
 		</div>
 
-
-		<ul class="side-nav" id="mobile-demo">
-			<li><a href="sass.html">Sass</a></li>
-			<li><a href="badges.html">Components</a></li>
-			<li><a href="collapsible.html">Javascript</a></li>
-			<li><a href="mobile.html">Mobile</a></li>
-		</ul>
 
 		<ul id="dropdown1" class="dropdown-content">
 			<li>
