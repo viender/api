afcd2cbc8def2aac3b6455804dbe846dc3c62465
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-03-04 23:16:52 +0700

    add users routes

--- src/viender/routes/api.php
+++ src/viender/routes/api.php
@@ -1,2 +1,7 @@
 <?php
+Route::group(['namespace' => 'Api'], function() {
+	Route::get('user', 'UsersController@authenticated')->name('api.viender.core.me');
 
+	// Models
+	Route::resource('users', 'UsersController', ['except' => ['create', 'edit'], 'as' => 'api.viender.core']);
+});
