7946d8418c6f71653f87d96eca43fbb01f7305e2
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-03-05 17:01:16 +0700

    install google analytic

--- viender/app/Http/Kernel.php
+++ viender/app/Http/Kernel.php
@@ -32,6 +32,8 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+
+            \App\Http\Middleware\AddVaryHeader::class,
         ],
 
         'api' => [
---
+++ viender/app/Http/Middleware/AddVaryHeader.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AddVaryHeader
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->header('Vary', 'User-Agent');
+
+        return $response;
+    }
+}
