1c602b679257475a100c693b0bd97a4e210ce048
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-03-07 22:45:30 +0700

    install summernote;

--- apps/socialite/src/Http/Controllers/AnswerController.php
+++ apps/socialite/src/Http/Controllers/AnswerController.php
@@ -5,6 +5,16 @@ namespace Viender\Socialite\Http\Controllers;
 
 class AnswerController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
 	public function index()
 	{
 		if(\Agent::isDesktop()) {
