616ab5045ffefe4dde5eab269eb91c7e8922eb19
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-04-02 02:47:08 +0700

    change theme color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#00acc1">
+    <meta name="theme-color" content="#f5f5f5">
     <meta name="mobile-web-app-capable" content="yes">
 
     <!-- CSRF Token -->
