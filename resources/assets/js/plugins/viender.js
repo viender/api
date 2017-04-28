b63c5f419edb31d0624befd63f5dc369d10df9c7
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-04-28 20:58:09 +0700

    use airbnb standar

--- resources/assets/js/plugins/viender.js
+++ resources/assets/js/plugins/viender.js
@@ -1,52 +1,54 @@
+/* eslint no-param-reassign: ["error", { "props": false } ] */
+
 import urlHelpers from '../helpers/url';
 import styleHelpers from '../helpers/style';
 import eventHelpers from '../helpers/event';
 
-window.guestUrls = [
-	url('login'),
-	url('register'),
+const guestUrls = [
+  window.url('login'),
+  window.url('register'),
 ];
 
 const userFetched = new Event('userFetched');
 
 export default {
-	install(Vue, options) {
-		if(! Vue.prototype.$viender) Vue.prototype.$viender = {};
+  install(Vue) {
+    if (!Vue.prototype.$viender) Vue.prototype.$viender = {};
 
-		Vue.prototype.$viender.treasure = window.treasure;
+    Vue.prototype.$viender.treasure = window.treasure;
 
-		Vue.prototype.$viender.helpers = Object.assign(
-			urlHelpers,
-            styleHelpers,
-            eventHelpers
-		);
+    Vue.prototype.$viender.helpers = Object.assign(
+      urlHelpers,
+      styleHelpers,
+      eventHelpers,
+    );
 
-		Vue.prototype.$viender.helpers.fetchAuthenticatedUser = function fetchAuthenticatedUser(argument) {
-			axios.get(api('/user'), {})
-			.then(function (response) {
-			    treasure.user = response.data;
-			    Vue.prototype.$viender.user = response.data;
-			    document.dispatchEvent(userFetched);
+    Vue.prototype.$viender.helpers.fetchAuthenticatedUser = function fetchAuthenticatedUser() {
+      axios.get(window.api('/user'), {})
+      .then((response) => {
+        treasure.user = response.data;
+        Vue.prototype.$viender.user = response.data;
+        document.dispatchEvent(userFetched);
 
-                if ('serviceWorker' in navigator) {
-                    window.addEventListener('load', function() {
-                        navigator.serviceWorker.register('/sw.js');
-                    });
-                }
-			})
-			.catch(function (error) {
-                if (error.response.status === 401) {
-                    navigator.serviceWorker.getRegistration().then((r) => {
-                        r.unregister();
-                    });
+        if ('serviceWorker' in navigator) {
+          window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js');
+          });
+        }
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          navigator.serviceWorker.getRegistration().then((r) => {
+            r.unregister();
+          });
 
-					if (guestUrls.indexOf(window.location.href) === -1) {
-                        document.location = url('login');
-					}
-			    }
-			});
-		}
+          if (guestUrls.indexOf(window.location.href) === -1) {
+            document.location = window.url('login');
+          }
+        }
+      });
+    };
 
-		Vue.prototype.$viender.helpers.fetchAuthenticatedUser();
-	}
-}
+    Vue.prototype.$viender.helpers.fetchAuthenticatedUser();
+  },
+};
