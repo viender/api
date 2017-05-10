4882f455589fe3c9a41b811500bfb064e56ee1b2
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-05-10 22:50:31 +0700

    hide mobile bottom navigation when typing

--- resources/assets/js/callbacks.js
+++ resources/assets/js/callbacks.js
@@ -2,5 +2,12 @@ $(document).ready(() => {
     console.log('document ready');
 	$('.button-collapse').sideNav();
 	$('.dropdown-button').dropdown();
-});
 
+    $('.form-control').focus(() => {
+        $('.navbar-mobile-menus').hide();
+    });
+
+    $('.form-control').blur(() => {
+        $('.navbar-mobile-menus').show();
+    });
+});
