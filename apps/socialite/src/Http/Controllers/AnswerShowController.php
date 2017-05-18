de06f7223cf169d891a1a4cb1df8bb4fa774dc33
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-05-18 20:16:47 +0700

    use auth middleware

--- apps/socialite/src/Http/Controllers/AnswerShowController.php
+++ apps/socialite/src/Http/Controllers/AnswerShowController.php
@@ -7,13 +7,23 @@ use Viender\Socialite\Models\Question;
 
 class AnswerShowController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
 	public function show(Question $question, $answerSlug)
 	{
 		$answer = Answer::where([
 			'question_id'	=> $question->id,
 			'slug'			=> $answerSlug,
 		])->firstOrFail();
-		
+
 		return view('viender.socialite.answerShow::index')->with(compact('answer'));
 	}
 }
