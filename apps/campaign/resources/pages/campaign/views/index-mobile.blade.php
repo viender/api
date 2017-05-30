b7e309a09429cf2b28debfe05d145784fa239a41
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-05-30 07:30:50 +0700

    add campaign mobile view

--- apps/campaign/resources/pages/campaign/views/index-mobile.blade.php
+++ apps/campaign/resources/pages/campaign/views/index-mobile.blade.php
@@ -1,22 +1,86 @@
 @extends('viender::layouts.app')
 
 @section('content')
-<div class="mobile">
-	<div class="row">
-		<div class="col s12">
-			<h5>Top stories for you</h5>
-		</div>
-	</div>
-	
-	<feed-list :feed-urls="{ answers: '{{ route('api.viender.socialite.answers.index') }}' }"></feed-list>
-	<answer-show-modal></answer-show-modal>
+<div class="" style="padding: 10px;">
+    <div>
+        <h5>Add New Campaigns</h5>
+
+        <form action="{{ route('web.viender.campaign.store') }}" method="POST">
+            {{ csrf_field() }}
+            <input type="text" name="name" placeholder="name" required="required">
+            <div class="input-field">
+                <select name="source" style="display: initial; width: 100%; margin-bottom: 10px;" required="required">
+                    <option value="" disabled selected>Source</option>
+                    <option value="Email">Email</option>
+                    <option value="Instagram">Instagram</option>
+                    <option value="Facebook">Facebook</option>
+                    <option value="Twitter">Twitter</option>
+                    <option value="Youtube">Youtube</option>
+                    <option value="Line">Line</option>
+                    <option value="WhatsApp">WhatsApp</option>
+                    <option value="Telegram">Telegram</option>
+                    <option value="Google Form">Google Form</option>
+                    <option value="Other">Other</option>
+                </select>
+            </div>
+            <div style="display: block; text-align: right;">
+                <input class="btn btn-default" type="submit">
+            </div>
+        </form>
+    </div>
+    <h5>Campaigns</h5>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Source</th>
+                <th>Hits</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($campaigns as $campaign)
+                <tr>
+                    <td>{{ $campaign->id }}</td>
+                    <td>{{ $campaign->name }}</td>
+                    <td>{{ $campaign->source }}</td>
+                    <td>{{ $campaign->campaignHits()->count() / 2 }}</td>
+                    <td>
+                        <div>
+                            @if(!$campaign->finish)
+                                <form style="display: inline-block;" action="{{ route('web.viender.campaign.reset', $campaign) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <input type="submit" value="Reset" onclick="return confirm('Are you sure to reset this Campaign?');">
+                                </form>
+                                <form style="display: inline-block;" action="{{ route('web.viender.campaign.finish', $campaign) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <input type="submit" value="Finish">
+                                </form>
+                            @else
+                                <form style="display: inline-block;" action="{{ route('web.viender.campaign.destroy', $campaign) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <input type="submit" value="Delete" onclick="return confirm('Are you sure to delete this Campaign?');">
+                                </form>
+                                <form style="display: inline-block;" action="{{ route('web.viender.campaign.unfinish', $campaign) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <input type="submit" value="Start Again">
+                                </form>
+                            @endif
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
 
 @section('head-styles')
-<link rel="stylesheet" href={{ mix('css/viender/socialite/read/app-mobile.css') }}>
+<link rel="stylesheet" href={{ mix('css/viender/socialite/read/app.css') }}>
 @endsection
 
 @section('scripts')
-<script src={{ mix('js/viender/socialite/read/app-mobile.js') }} async defer></script>
+<script src={{ mix('js/viender/socialite/read/app.js') }} async defer></script>
 @endsection
