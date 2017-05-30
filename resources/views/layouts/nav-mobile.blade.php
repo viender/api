3babdeb2c4f21f045ef89a03e1dd62b186c3398f
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-05-30 20:23:53 +0700

    add 404 page

--- resources/views/layouts/nav-mobile.blade.php
+++ resources/views/layouts/nav-mobile.blade.php
@@ -18,65 +18,60 @@
                 </div>
             </div>
             <div class="row navbar-mobile-menus">
-                @if (Auth::guest())
-                    {{-- <div class="navbar-mobile-menus-button col s6"><a href="{{ url('/login') }}">Login</a></div>
-                    <div class="navbar-mobile-menus-button col s6"><a href="{{ url('/register') }}">Register</a></div> --}}
-                @else
-                    <div class="navbar-button col s3 {{ Route::currentRouteName() == 'web.viender.socialite.pages.read' ? 'active' : '' }}"
-                        :class="$store.state.navigation.activeMenu === 1 ? 'active' : ''">
-                        <a
-                            id="read-menu"
-                            @click="$store.commit('navigation/SET_ACTIVE_MENU', {activeMenu: 1})"
-                            :class="$store.state.navigation.activeMenu === 1 ? 'active' : ''"
-                            class="navbar-mobile-menus-button {{ Route::currentRouteName() == 'web.viender.socialite.pages.read' ? 'active' : '' }}"
-                            href={{ route('web.viender.socialite.pages.read') }}>
-                            <i class="fa fa-newspaper-o" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                    <div class="navbar-button col s3 {{ Route::currentRouteName() == 'web.viender.socialite.pages.answer' ? 'active' : '' }}"
-                        :class="$store.state.navigation.activeMenu === 2 ? 'active' : ''">
-                        <a
-                            id="answer-menu"
-                            @click="$store.commit('navigation/SET_ACTIVE_MENU', {activeMenu: 2})"
-                            :class="$store.state.navigation.activeMenu === 2 ? 'active' : ''"
-                            class="navbar-mobile-menus-button {{ Route::currentRouteName() == 'web.viender.socialite.pages.answer' ? 'active' : '' }}"
-                            href={{ route('web.viender.socialite.pages.answer') }}>
-                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                    <div class="navbar-button notification-menu-button col s3 {{ Route::currentRouteName() == 'web.viender.notification.notification.index' ? 'active' : '' }}"
-                        :class="$store.state.navigation.activeMenu === 3 ? 'active' : ''">
-                        @if(\Auth::user())
-                            @if(Route::currentRouteName() !== 'web.viender.notification.notification.index' && \Auth::user()->notifications()->count() > 0)
-                                <div
-                                    onclick={{ 'window.location.href=' . '"' . route('web.viender.notification.notification.index') . '"' }}
-                                    class="notification-count"
-                                    v-html="$store.state.navigation.notificationCount"
-                                    v-cloak="true"
-                                    v-if="$store.state.navigation.showNotificationCount && $store.state.navigation.notificationCount"></div>
-                            @endif
+                <div class="navbar-button col s3 {{ Route::currentRouteName() == 'web.viender.socialite.pages.read' ? 'active' : '' }}"
+                    :class="$store.state.navigation.activeMenu === 1 ? 'active' : ''">
+                    <a
+                        id="read-menu"
+                        @click="$store.commit('navigation/SET_ACTIVE_MENU', {activeMenu: 1})"
+                        :class="$store.state.navigation.activeMenu === 1 ? 'active' : ''"
+                        class="navbar-mobile-menus-button {{ Route::currentRouteName() == 'web.viender.socialite.pages.read' ? 'active' : '' }}"
+                        href={{ route('web.viender.socialite.pages.read') }}>
+                        <i class="fa fa-newspaper-o" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="navbar-button col s3 {{ Route::currentRouteName() == 'web.viender.socialite.pages.answer' ? 'active' : '' }}"
+                    :class="$store.state.navigation.activeMenu === 2 ? 'active' : ''">
+                    <a
+                        id="answer-menu"
+                        @click="$store.commit('navigation/SET_ACTIVE_MENU', {activeMenu: 2})"
+                        :class="$store.state.navigation.activeMenu === 2 ? 'active' : ''"
+                        class="navbar-mobile-menus-button {{ Route::currentRouteName() == 'web.viender.socialite.pages.answer' ? 'active' : '' }}"
+                        href={{ route('web.viender.socialite.pages.answer') }}>
+                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="navbar-button notification-menu-button col s3 {{ Route::currentRouteName() == 'web.viender.notification.notification.index' ? 'active' : '' }}"
+                    :class="$store.state.navigation.activeMenu === 3 ? 'active' : ''">
+                    @if(\Auth::user())
+                        @if(Route::currentRouteName() !== 'web.viender.notification.notification.index' && \Auth::user()->notifications()->count() > 0)
+                            <div
+                                onclick={{ 'window.location.href=' . '"' . route('web.viender.notification.notification.index') . '"' }}
+                                class="notification-count"
+                                v-html="$store.state.navigation.notificationCount"
+                                v-cloak="true"
+                                v-if="$store.state.navigation.showNotificationCount && $store.state.navigation.notificationCount"></div>
                         @endif
-                        <a
-                            id="notifications-menu"
-                            @click="$store.commit('navigation/SET_ACTIVE_MENU', {activeMenu: 3})"
-                            :class="$store.state.navigation.activeMenu === 3 ? 'active' : ''"
-                            class="navbar-mobile-menus-button {{ Route::currentRouteName() == 'web.viender.notification.notification.index' ? 'active' : '' }}"
-                            href={{ route('web.viender.notification.notification.index') }}>
-                            <i class="fa fa-bell" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                    <div class="navbar-button col s3 {{ Route::currentRouteName() == 'web.viender.profile.pages.profile' ? 'active' : '' }}"
-                        :class="$store.state.navigation.activeMenu === 4 ? 'active' : ''">
-                        <a
-                            id="profile-menu"
-                            @click="$store.commit('navigation/SET_ACTIVE_MENU', {activeMenu: 4})"
-                            :class="$store.state.navigation.activeMenu === 4 ? 'active' : ''"
-                            class="navbar-mobile-menus-button {{ Route::currentRouteName() == 'web.viender.profile.pages.profile' ? 'active' : '' }}"
-                            href={{ route('web.viender.profile.pages.profile', \Auth::user()->username) }}>
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                @endif
+                    @endif
+                    <a
+                        id="notifications-menu"
+                        @click="$store.commit('navigation/SET_ACTIVE_MENU', {activeMenu: 3})"
+                        :class="$store.state.navigation.activeMenu === 3 ? 'active' : ''"
+                        class="navbar-mobile-menus-button {{ Route::currentRouteName() == 'web.viender.notification.notification.index' ? 'active' : '' }}"
+                        href={{ route('web.viender.notification.notification.index') }}>
+                        <i class="fa fa-bell" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="navbar-button col s3 {{ Route::currentRouteName() == 'web.viender.profile.pages.profile' ? 'active' : '' }}"
+                    :class="$store.state.navigation.activeMenu === 4 ? 'active' : ''">
+                    <a
+                        id="profile-menu"
+                        @click="$store.commit('navigation/SET_ACTIVE_MENU', {activeMenu: 4})"
+                        :class="$store.state.navigation.activeMenu === 4 ? 'active' : ''"
+                        class="navbar-mobile-menus-button {{ Route::currentRouteName() == 'web.viender.profile.pages.profile' ? 'active' : '' }}"
+                        href={{ \Auth::user() ? route('web.viender.profile.pages.profile', \Auth::user()->username) : url('/') }}>
+                        <i class="fa fa-user" aria-hidden="true"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
---
+++ viender/resources/views/errors/404.blade.php
@@ -0,0 +1,33 @@
+@extends('viender::layouts.app')
+
+@section('content')
+<div class="" style="padding: 10px;">
+    @if(\Agent::isDesktop())
+        <div class="row">
+            <div class="col s3">
+
+            </div>
+            <div class="col s6">
+                <h5>Page Not Found</h5>
+                <p>We searched everywhere but couldn't find the page you were looking for.</p>
+                {{-- <a href="{{ url()->previous() }}">Go Back</a>
+                <a href="{{ url()->previous() }}">Viender Home</a> --}}
+            </div>
+            <div class="col s3">
+
+            </div>
+        </div>
+    @else
+        <h5>Page Not Found</h5>
+        <p>We searched everywhere but couldn't find the page you were looking for.</p>
+    @endif
+</div>
+@endsection
+
+@section('head-styles')
+<link rel="stylesheet" href={{ mix('css/viender/socialite/read/app.css') }}>
+@endsection
+
+@section('scripts')
+<script src={{ mix('js/viender/socialite/read/app.js') }} async defer></script>
+@endsection
