72aa918d7fc49153a2302c176ea4268fba7a957f
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-06-01 06:12:51 +0700

    update

--- viender/app/Exceptions/Handler.php
+++ viender/app/Exceptions/Handler.php
@@ -79,6 +79,6 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return redirect()->guest('login');
+        return redirect()->guest(route('login'));
     }
 }
