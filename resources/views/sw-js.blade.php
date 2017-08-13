d32e21e43fd09e531b30830254240ee3d2f13f6a
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2017-08-13 10:33:54 +0700

    update sw cache version

--- resources/views/sw-js.blade.php
+++ resources/views/sw-js.blade.php
@@ -10,7 +10,7 @@ importScripts('/js/vendor/sw-toolbox/sw-toolbox.js');
 toolbox.options.debug = true;
 @endif
 
-toolbox.options.cache.name = 'viender_v0.1.8';
+toolbox.options.cache.name = 'viender_v0.1.9';
 
 toolbox.precache([
     '{{ $mixManifest['/js/core.js'] }}',
